02aea665c38314b291157855dfd25adf32c56e4d
Nelson Cárdenas Bolaño <nelsoncardenas28@gmail.com>
2022-07-07 17:49:35 -0500

    Update requirements path and testing.yml

--- utilities/setter.py
+++ utilities/setter.py
@@ -5,7 +5,7 @@ from base64 import b64decode
 def main():
     key = os.environ.get("SERVICE_ACCOUNT_KEY")
     with open("path.json", "w") as json_file:
-        json_file.write(b64decode(key.decode()))
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath("path.json"))
 
 
